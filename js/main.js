6fbe0d732ccd09f1e059502b023b0a37e443d04d
andrezzz2312 <andrezzz2312@gmail.com>
2023-07-05 19:06:34 -0500

    version sin delays

--- js/main.js
+++ js/main.js
@@ -233,7 +233,7 @@ function InterpolateVideo(videoToPause, videoToVanish, videoToPlay) {
 	videoToPlay.style.opacity = 1
 	setTimeout(() => {
 		videoToPlay.play()
-	}, 250)
+	}, 0)
 }
 
 // loop.currentTime = 60
@@ -630,7 +630,7 @@ function createContent(
 										// createBackButton()
 										HideShowCont()
 									})
-								}, 500)
+								}, 0)
 							}
 						}
 					}
@@ -784,7 +784,7 @@ function backButtonFunction() {
 			video1.remove()
 			video2.remove()
 			video3.remove()
-		}, 500)
+		}, 0)
 
 		HideShowMainButtons()
 
@@ -819,7 +819,7 @@ function backButtonFunctionFromBack() {
 		HideShowMainButtons()
 		setTimeout(() => {
 			loop.play()
-		}, 500)
+		}, 0)
 		subVideoTurn.classList.add('short-vanish')
 		console.log(pageIndex)
 		loop.style.zIndex = '-1'
@@ -1025,7 +1025,7 @@ loop.addEventListener('loadedmetadata', function (e) {
 	initial.classList.add('short-vanish')
 	setTimeout(() => {
 		initial.style.zIndex = '-200'
-	}, 500)
+	}, 0)
 })
 
 if (loop.readyState >= 1) {
@@ -1037,7 +1037,7 @@ if (loop.readyState >= 1) {
 	initial.classList.add('short-vanish')
 	setTimeout(() => {
 		initial.style.zIndex = '-200'
-	}, 500)
+	}, 0)
 }
 
 window.addEventListener('DOMContentLoaded', function () {
@@ -1116,7 +1116,7 @@ mainMenuB.forEach((e, i) => {
 		if (showCont.innerHTML !== '') {
 			setTimeout(() => {
 				showCont.innerHTML = ''
-			}, 500)
+			}, 0)
 		}
 
 		createContent(
@@ -1233,7 +1233,7 @@ mainMenuB.forEach((e, i) => {
 							}
 							HideShowCont()
 						})
-					}, 500)
+					}, 0)
 				}
 			}
 		}
@@ -1250,7 +1250,7 @@ var SirvOptions = {
 			loader.style.zIndex = '-100'
 			setTimeout(() => {
 				initial.style.zIndex = '-200'
-			}, 300)
+			}, 0)
 		},
 	},
 }
